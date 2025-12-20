3932f8e8114018ce2904e3774a3cb28ea7305fdc
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2025-12-20 16:22:39 +0100

    Made a simple test for parsing a valid RSS feed

---
+++ src/utils/parser.rs
@@ -0,0 +1,19 @@
+use std::fs::File;
+use std::io::{BufReader, Error};
+use rss::Channel;
+
+pub trait Parser<T> {
+    fn parse(filepath: &str) -> Result<Vec<T>, Error>;
+}
+
+pub struct FeedParser;
+
+#[test]
+fn parse_valid_xml(){
+    let file = File::open("tests/valid_feed.xml").unwrap();
+    let channel = Channel::read_from(BufReader::new(file)).unwrap();
+    channel.write_to(::std::io::sink()).unwrap();
+    let string = channel.to_string();
+    println!("{}", string);
+}
+
