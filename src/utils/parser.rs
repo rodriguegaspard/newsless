88b1c37e17a5c887592edd69919b0b5b8e5d3d9e
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2025-12-20 16:25:30 +0100

    Applied cargo fmt

--- src/utils/parser.rs
+++ src/utils/parser.rs
@@ -1,6 +1,6 @@
+use rss::Channel;
 use std::fs::File;
 use std::io::{BufReader, Error};
-use rss::Channel;
 
 pub trait Parser<T> {
     fn parse(filepath: &str) -> Result<Vec<T>, Error>;
@@ -9,11 +9,10 @@ pub trait Parser<T> {
 pub struct FeedParser;
 
 #[test]
-fn parse_valid_xml(){
+fn parse_valid_xml() {
     let file = File::open("tests/valid_feed.xml").unwrap();
     let channel = Channel::read_from(BufReader::new(file)).unwrap();
     channel.write_to(::std::io::sink()).unwrap();
     let string = channel.to_string();
     println!("{}", string);
 }
-
